866e6ed717329a3315b0c1787a7663f953dea98f
Алексей Писарев <afonasiu2983@gmail.com>
2022-09-22 15:56:54 +1000

    Dobavil knopky

--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,10 @@ namespace Pisarev
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button1.Text = "hello";
+        }
     }
 }
